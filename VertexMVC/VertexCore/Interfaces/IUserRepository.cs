37b800269aa58a41ba5dd63d36ff685aedf2df60
Mac <mac@Macs-MacBook-Pro-2.local>
2022-09-29 16:44:45 +0100

    create user repository

---
+++ VertexMVC/VertexCore/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VertexCore.Models;
+
+namespace VertexCore.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<bool> AddUserAsync(User model);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetAUserAsync(string Id);
+    }
+}
---
+++ VertexMVC/VertexInfrastrature/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VertexCore.Interfaces;
+using VertexCore.Models;
+
+namespace VertexInfrastrature.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddUserAsync(User model)
+        {
+            await _context.AddAsync(model);
+
+            return await SaveAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        public async Task<User> GetAUserAsync(string Id)
+        {
+            var user = await _context.Users
+                        .Where(x => x.Id == Id)
+                        .FirstOrDefaultAsync();
+
+            return user;
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
