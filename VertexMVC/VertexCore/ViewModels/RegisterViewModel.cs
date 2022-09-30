56782519bb63738ba82b093680e61aa1b0f95bd9
Mac <mac@Macs-MacBook-Pro-2.local>
2022-09-30 04:50:27 +0100

    modify view model

--- VertexMVC/VertexCore/ViewModels/RegisterViewModel.cs
+++ VertexMVC/VertexCore/ViewModels/RegisterViewModel.cs
@@ -5,23 +5,23 @@ namespace VertexCore.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required (ErrorMessage = "FirstName is Required")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "LastName is Required")]
         public string LastName { get; set; }
        
         public string MiddleName { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "FirstName is Required")]
+        [EmailAddress(ErrorMessage = "Please enter the right format like xyz@gmail.com")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Address is Required")]
         public string Address { get; set; }
-        [Required]
+        [Required(ErrorMessage = "City is Required")]
         public string City { get; set; }
 
         public string Zip { get; set; }
-        [Required]
+        [Required(ErrorMessage = "FirstName is Required")]
         [Phone]
         public string Phone { get; set; }
     }
